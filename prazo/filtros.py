abb7b5783452208dfb0ecc421181ad211aa0233b
victormellogab <victorm.mello@grupoaguasdobrasil.com.br>
2025-11-24 11:34:17 -0300

    arquivo responsável por criar os filtros do período e de área executora e solicitante

--- prazo/filtros.py
+++ prazo/filtros.py
@@ -5,5 +5,5 @@ def filtrar_periodo(df, coluna_data, data_inicio, data_fim):
 
 def excluir_invalidos(df):
     df = df[(df['AREA_EXEC'] != 'ENGENHARIA') & (df['Área Exec.'] != 'ENGENHARIA')]
-    df = df[df['DH_EXEC_INI_REL'].notna() & df['DH_EXEC_FIM_REL'].notna()]
     return df
+
