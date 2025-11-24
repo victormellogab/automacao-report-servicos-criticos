b179e5fee6de92745aa71c96ed0e81d244ae9db7
victormellogab <victorm.mello@grupoaguasdobrasil.com.br>
2025-11-24 11:31:20 -0300

    define caminhos, saídas e concessionárias

--- prazo/config.py
+++ prazo/config.py
@@ -1,8 +1,8 @@
 import os
 
 # Caminhos dos arquivos
-CAMINHO_INOVA = r"C:\Users\victor.mello\OneDrive - Grupo Aguas do Brasil\Área de Trabalho\Script Geração Report Serviços\Dados\data.csv"
-CAMINHO_SERVICOS = r"C:\Users\victor.mello\OneDrive - Grupo Aguas do Brasil\Área de Trabalho\Script Geração Report Serviços\Dados\TODOS_SERVICOS.csv"
+CAMINHO_INOVA = r"C:\dados-report\base_report_outubro.xlsx"
+CAMINHO_SERVICOS = r"C:\dados-report\TODOS_SERVICOS.csv"
 
 # Pasta de saída
 PASTA_SAIDA = r'H:\COMERCIAL\MEDIÇÃO\ARQUIVO MORTO\BERNARDO\GSC\Automação Report'
@@ -10,17 +10,17 @@ os.makedirs(PASTA_SAIDA, exist_ok=True)
 
 # Lista de concessionárias
 CONCESSIONARIAS = [
-    #'CAAN',
+    'CAAN',
     'CAC',
-    #'CAI',
-    #'CAIZ',
-    #'CAJ',
-    #'CAJA',
-    #'CAN',
-    #'CANF',
-    #'CAP',
-    #'CAPAM',
-    #'CAPY',
-    #'CAV',
+    'CAI',
+    'CAIZ',
+    'CAJ',
+    'CAJA',
+    'CAN',
+    'CANF',
+    'CAP',
+    'CAPAM',
+    'CAPY',
+    'CAV',
     #'RIOMAIS'
 ]
