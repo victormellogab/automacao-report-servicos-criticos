06cc3ab30ce931c2e57accaea553024ee3992d64
victormellogab <victorm.mello@grupoaguasdobrasil.com.br>
2025-11-24 14:09:47 -0300

    centraliza caminhos de entrada e saida, define concessionarias

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+# Caminhos dos arquivos
+CAMINHO_INOVA = r"C:\dados-report\base_report_outubro.xlsx"
+CAMINHO_SERVICOS = r"C:\dados-report\TODOS_SERVICOS.csv"
+
+# Pasta de saída
+PASTA_SAIDA = r'H:\COMERCIAL\MEDIÇÃO\ARQUIVO MORTO\BERNARDO\GSC\Automação Report'
+os.makedirs(PASTA_SAIDA, exist_ok=True)
+
+# Lista de concessionárias
+CONCESSIONARIAS = [
+    'CAAN',
+    'CAC',
+    'CAI',
+    'CAIZ',
+    'CAJ',
+    'CAJA',
+    'CAN',
+    'CANF',
+    'CAP',
+    'CAPAM',
+    'CAPY',
+    'CAV',
+    #'RIOMAIS'
+]
