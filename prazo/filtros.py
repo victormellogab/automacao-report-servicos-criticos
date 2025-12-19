81b03592945836f69b953f56872c8ac41ab1462f
victormellogab <victorm.mello@grupoaguasdobrasil.com.br>
2025-12-19 16:43:51 -0300

    ajustes filtros

--- prazo/filtros.py
+++ prazo/filtros.py
@@ -3,7 +3,7 @@ import pandas as pd
 def filtrar_periodo(df, coluna_data, data_inicio, data_fim):
     return df[(df[coluna_data] >= data_inicio) & (df[coluna_data] <= data_fim)]
 
-def excluir_invalidos(df):
+'''def excluir_invalidos(df):
     df = df[(df['AREA_EXEC'] != 'ENGENHARIA') & (df['Área Exec.'] != 'ENGENHARIA')]
     return df
-
+'''
